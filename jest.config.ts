30c12645a579d264cd784eb8f431bb6bbdeec13c
Rodrigo Dorat <rdoratm@gmail.com>
2022-07-07 23:48:04 -0400

    :wrench: Added jest config file to setup testing environment

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  testMatch: ['**/?(*.)+(spec|test).+(ts)'],
+  transform: {
+    '^.+.(ts)$': 'ts-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/*.ts',
+    '!src/main.ts',
+    '!**/*.enum.ts',
+    '!**/*.interface.ts',
+    '!**/*.module.ts',
+  ],
+  testPathIgnorePatterns: ['node_modules'],
+  coveragePathIgnorePatterns: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  coverageReporters: ['text', 'lcov'],
+  reporters: ['default'],
+};
+
+export default config;
