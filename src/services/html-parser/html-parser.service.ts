1a89d4442f0eea2692e59083b921eeb0667835ee
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-07 23:43:23 -0300

    :test_tube: added unit test for html parser

--- src/services/html-parser/html-parser.service.ts
+++ src/services/html-parser/html-parser.service.ts
@@ -3,8 +3,8 @@ import { JSDOM } from 'jsdom';
 
 @Injectable()
 export class HtmlParserService {
-    public async parseHtml(content: string): Promise<Document>{
-        const dom = new JSDOM(content);
-        return dom.window.document;
-    }
+  public async parseHtml(content: string): Promise<Document> {
+    const dom = new JSDOM(content);
+    return dom.window.document;
+  }
 }
