b50d8684dafcce3e9a933aec8ccbbbc4783f0672
Chelosky <marcelolazo19972013@hotmail.com>
2022-10-26 22:27:14 -0300

    :sparkles: added new methods for search mangas process for tmo lector net

--- src/modules/tmo-lector-net/tmo-lector-net.controller.ts
+++ src/modules/tmo-lector-net/tmo-lector-net.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseBoolPipe,
+  Query,
+} from '@nestjs/common';
 import { TmoLectorNetService } from './tmo-lector-net.service';
 
 @Controller('tmo-lector-net')
@@ -9,5 +16,13 @@ export class TmoLectorNetController {
   public async getManga(): Promise<any> {
     const url = 'https://tmolector.net/manga/one-punch-man';
     return this.tmoLectorNetService.getPage(url);
+  }
+
+  @Get('search/:manga')
+  public async searchManga(
+    @Param() params: any,
+    @Query('getAll', new DefaultValuePipe(false), ParseBoolPipe) getAll = false,
+  ): Promise<any> {
+    return this.tmoLectorNetService.search(params.manga, getAll);
   }
 }
--- src/modules/tmo-lector-net/tmo-lector-net.service.ts
+++ src/modules/tmo-lector-net/tmo-lector-net.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import _ from 'lodash';
 import { HttpService } from '../../services/http/http.service';
 import { HtmlParserService } from '../../services/html-parser/html-parser.service';
 
@@ -8,6 +9,55 @@ export class TmoLectorNetService {
     private readonly httpService: HttpService,
     private readonly htmlParser: HtmlParserService,
   ) {}
+
+  public async search(value: string, getAll = false) {
+    const { results, hasMorePages, totalPages } =
+      await this._getMangasFromSearch(value, 1);
+
+    if (getAll && hasMorePages) {
+      const searchPromises = _.range(2, totalPages + 1).map(
+        async (pageNbr: number) => {
+          const response = await this._getMangasFromSearch(value, pageNbr);
+          return response.results;
+        },
+      );
+      const responses = await Promise.all(searchPromises);
+      results.push(..._.flattenDeep(responses));
+    }
+
+    return results;
+  }
+
+  private async _getMangasFromSearch(value: string, pageNbr: number) {
+    const base_url = 'https://tmolector.net/biblioteca';
+    const params = { query: { search: value, page: pageNbr }, url: base_url };
+    const { body } = await this.httpService.get(params);
+    const document = await this.htmlParser.parseHtml(body);
+    const results = [
+      ...document.querySelectorAll(
+        '.manga_portada > .page-item-detail > .manga_biblioteca > a',
+      ),
+    ].map((element: Element) => {
+      const url = element.getAttribute('href');
+      const name = element.getAttribute('title');
+      const thumbnail = element.querySelector('img').getAttribute('src');
+      return {
+        url,
+        name,
+        thumbnail,
+      };
+    });
+    const { length, [length - 2]: totalPages } = [
+      ...document.querySelectorAll('.pagination > .page-item'),
+    ].map((item: Element) => Number(item.textContent));
+    return {
+      hasMorePages: !!totalPages,
+      hasPreviousPage: totalPages ? 1 < pageNbr : false,
+      hasNextPage: totalPages ? pageNbr < totalPages : false,
+      totalPages,
+      results,
+    };
+  }
 
   public async getPage(url: string) {
     const chapters = await this._getChapters(url);
