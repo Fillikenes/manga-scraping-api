92b855c97ddcdd33380f3c896c614bf492ed4823
Rodrigo Dorat <rdoratm@gmail.com>
2022-11-20 22:13:00 -0300

    :test_tube: Added providers to tests

--- src/modules/manga-tigre/manga-tigre.controller.spec.ts
+++ src/modules/manga-tigre/manga-tigre.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MangaTigreController } from './manga-tigre.controller';
+import { MangaTigreService } from './manga-tigre.service';
 
 describe('MangaTigreController', () => {
   let controller: MangaTigreController;
+  let service: MangaTigreService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MangaTigreController],
+      providers: [
+        {
+          provide: MangaTigreService,
+          useValue: {
+            getManga: jest.fn(),
+            searchManga: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MangaTigreController>(MangaTigreController);
--- src/modules/manga-tigre/manga-tigre.service.spec.ts
+++ src/modules/manga-tigre/manga-tigre.service.spec.ts
@@ -1,15 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MangaTigreService } from './manga-tigre.service';
+import { HttpService } from '../../services/http/http.service';
+import { PuppeteerService } from '../../services/puppeteer/puppeteer.service';
+
+jest.mock('puppeteer', () => ({
+  Page: jest.fn().mockImplementation(() => ({
+    $$eval: jest.fn(),
+    url: jest.fn().mockReturnValue(''),
+  })),
+  Browser: jest.fn(),
+}));
 
 describe('MangaTigreService', () => {
   let service: MangaTigreService;
+  let httpService: HttpService;
+  let puppeteerService: PuppeteerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MangaTigreService],
+      providers: [
+        MangaTigreService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+        {
+          provide: PuppeteerService,
+          useValue: {
+            createBrowser: jest.fn(),
+            goToPage: jest.fn(),
+            clearCookies: jest.fn(),
+            closeBrowser: jest.fn(),
+            closePage: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MangaTigreService>(MangaTigreService);
+    httpService = module.get<HttpService>(HttpService);
+    puppeteerService = module.get<PuppeteerService>(PuppeteerService);
   });
 
   it('should be defined', () => {
