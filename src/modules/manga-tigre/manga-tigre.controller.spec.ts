6bf00530df11a3074811675a3dd9f8b712e2772a
Rodrigo Dorat <rdoratm@gmail.com>
2022-11-30 22:29:28 -0300

    :test_tube: added test for http service and manga tigre controlle

--- src/modules/manga-tigre/manga-tigre.controller.spec.ts
+++ src/modules/manga-tigre/manga-tigre.controller.spec.ts
@@ -30,4 +30,57 @@ describe('MangaTigreController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('#getManga - should get the data of a manga of manga tigre site based in manga name given as argument', async () => {
+    const params = {
+      manga: 'one punch man',
+    };
+    const expectedResponse = {
+      chapters: [
+        {
+          id: 1,
+          name: 'one punch',
+          images: [],
+        },
+      ],
+    };
+    const getImagesChapterSpy = jest
+      .spyOn(service, 'getManga')
+      .mockResolvedValue(expectedResponse);
+
+    const result = await controller.getManga(params);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(expectedResponse);
+    expect(getImagesChapterSpy).toHaveBeenCalledWith(params.manga);
+  });
+
+  it('#searchManga - should get list of a mangas from manga tigre site based in manga name given as argument', async () => {
+    const params = {
+      manga: 'one p',
+    };
+    const expectedResponse = {
+      chapters: [
+        {
+          id: 1,
+          name: 'one punch man',
+          slug: 'one-punch-man',
+        },
+        {
+          id: 2,
+          name: 'one piece',
+          slug: 'one-piece',
+        },
+      ],
+    };
+    const getImagesChapterSpy = jest
+      .spyOn(service, 'searchManga')
+      .mockResolvedValue(expectedResponse);
+
+    const result = await controller.searchManga(params);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(expectedResponse);
+    expect(getImagesChapterSpy).toHaveBeenCalledWith(params.manga);
+  });
 });
