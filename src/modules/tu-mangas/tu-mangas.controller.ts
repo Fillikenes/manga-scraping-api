74b84aac7bc7e0ca2085349b56d0772dbd57b87c
NicolasHCastillo <nicolashidcas@gmail.com>
2023-01-20 21:55:11 -0300

    :sparkles: modificated the function response to get and search manga from TuMangas module

--- src/modules/tu-mangas/tu-mangas.controller.ts
+++ src/modules/tu-mangas/tu-mangas.controller.ts
@@ -2,16 +2,23 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { TuMangasGetListDto } from './dto';
 import { TuMangasService } from './tu-mangas.service';
 import { TuMangasSearchDto } from './dto/tu-mangas-query.dto';
+import {
+  IBaseController,
+  IOutboundChapter,
+  IOutboundSearchResponse,
+} from 'src/interfaces';
 
 @Controller('tu-mangas')
-export class TuMangasController {
+export class TuMangasController implements IBaseController {
   constructor(private readonly tumangasService: TuMangasService) {}
   @Get('/')
-  async getListCharacters(@Query() query: TuMangasGetListDto): Promise<any> {
+  async get(@Query() query: TuMangasGetListDto): Promise<IOutboundChapter[]> {
     return this.tumangasService.getMangaInfo(query.name);
   }
   @Get('/search/:nameManga')
-  async searchManga(@Param() params: TuMangasSearchDto): Promise<any> {
+  async search(
+    @Param() params: TuMangasSearchDto,
+  ): Promise<IOutboundSearchResponse[]> {
     return this.tumangasService.searchManga(params.nameManga);
   }
 }
